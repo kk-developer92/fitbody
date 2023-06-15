eaedce46e1c1e80349634120b570f32a723d552d
Константин Кузнецов <kk.developer92@MacBook-Air-Konstantin.local>
2023-06-15 21:54:38 +0500

    fix add bootstrap js

--- front/nuxt.config.ts
+++ front/nuxt.config.ts
@@ -2,25 +2,8 @@
 export default defineNuxtConfig({
   // @ts-ignore
   devtools: { enabled: false },
-  head: {
-    //...
-    link: [
-      {
-        rel: 'stylesheet',
-        href: 'https://fonts.googleapis.com/css2?family=Ralevay'
-      }
-    ]
-  },
+
   buildModules: [
     '@nuxtjs/style-resources',
   ],
-
-  css: [
-    "@/node_modules/bootstrap/dist/css/bootstrap.min.css"
-  ],
-  script: [
-    {
-      src: "@/node_modules/bootstrap/dist/js/bootstrap.bundle.js"
-    }
-  ]
 })
