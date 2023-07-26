26b3f3fc3ab19943e15a0dfeaefd592d3931e387
Konstantin <kk.developer92@gmail.com>
2023-07-26 20:16:17 +0500

    fancybox test

--- pages/index.vue
+++ pages/index.vue
@@ -178,7 +178,7 @@
 
 <script lang="ts" setup>
 import 'swiper/css';
-// import {Fancybox} from "@fancyapps/ui";
+import {Fancybox} from "@fancyapps/ui";
 import '@fancyapps/ui/dist/fancybox/fancybox.css';
 import { courses, nutrions, trainings } from "~/data";
 import TabHeader from "~/components/common/TabHeader.vue";
@@ -272,7 +272,7 @@ const sections = ref([
     }
 ])
 
-// Fancybox.bind("[data-fancybox]");
+Fancybox.bind("[data-fancybox]");
 
 </script>
 
