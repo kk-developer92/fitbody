8c7d54f3b92057a884b90a3175938686348b7a24
Konstantin <kk.developer92@gmail.com>
2023-09-08 14:35:31 +0500

    account page front

--- pages/account/index.vue
+++ pages/account/index.vue
@@ -1,21 +1,26 @@
 <template>
 	<!-- Navbar -->
-	<nav class="navbar navbar-expand-lg navbar-light bg-dark pt-2">
+	<nav id="main-navbar " class="navbar navbar-expand-lg navbar-light bg-dark pt-2">
 		<!-- Container wrapper -->
 		<div class="container-fluid">
 			<!-- Toggle button -->
 			<button class="navbar-toggler" type="button" data-mdb-toggle="collapse"
 				data-mdb-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
 				aria-label="Toggle navigation">
-				<i class="fas fa-bars"></i>
+				<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+					class="bi bi-three-dots-vertical" viewBox="0 0 16 16">
+					<path
+						d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z" />
+				</svg>
 			</button>
 
 			<!-- Collapsible wrapper -->
 			<div class="collapse navbar-collapse" id="navbarSupportedContent">
 				<!-- Navbar brand -->
-				<a class="navbar-brand mt-2 mt-lg-0" href="#">
-					<img src="https://mdbcdn.b-cdn.net/img/logo/mdb-transaprent-noshadows.webp" height="15" alt="MDB Logo"
-						loading="lazy" />
+				<a class="navbar-brand mt-2 mt-lg-0 bg-light" href="#">
+					<img src="@/assets/appImages/logo.png" height="47" alt="MDB Logo" loading="lazy" />
+
+
 				</a>
 				<!-- Left links -->
 				<ul class="navbar-nav me-auto mb-2 mb-lg-0">
@@ -26,7 +31,7 @@
 						<a class="nav-link" href="#">Результаты</a>
 					</li>
 					<li class="nav-item">
-						<a class="nav-link" href="#">Еще что-то	</a>
+						<a class="nav-link" href="#">Еще что-то </a>
 					</li>
 				</ul>
 				<!-- Left links -->
@@ -35,34 +40,7 @@
 
 			<!-- Right elements -->
 			<div class="d-flex align-items-center">
-				<!-- Icon -->
-				<a class="text-reset me-3" href="#">
-					<i class="fas fa-shopping-cart"></i>
-				</a>
 
-				<!-- Notifications -->
-				<div class="dropdown">
-					<a class="text-reset me-3 dropdown-toggle hidden-arrow" href="#" id="navbarDropdownMenuLink"
-						role="button" data-mdb-toggle="dropdown" aria-expanded="false">
-						<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-							class="bi bi-bell" viewBox="0 0 16 16">
-							<path
-								d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2zM8 1.918l-.797.161A4.002 4.002 0 0 0 4 6c0 .628-.134 2.197-.459 3.742-.16.767-.376 1.566-.663 2.258h10.244c-.287-.692-.502-1.49-.663-2.258C12.134 8.197 12 6.628 12 6a4.002 4.002 0 0 0-3.203-3.92L8 1.917zM14.22 12c.223.447.481.801.78 1H1c.299-.199.557-.553.78-1C2.68 10.2 3 6.88 3 6c0-2.42 1.72-4.44 4.005-4.901a1 1 0 1 1 1.99 0A5.002 5.002 0 0 1 13 6c0 .88.32 4.2 1.22 6z" />
-						</svg>
-						<span class="badge rounded-pill badge-notification bg-danger">1</span>
-					</a>
-					<ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownMenuLink">
-						<li>
-							<a class="dropdown-item" href="#">Some news</a>
-						</li>
-						<li>
-							<a class="dropdown-item" href="#">Another news</a>
-						</li>
-						<li>
-							<a class="dropdown-item" href="#">Something else here</a>
-						</li>
-					</ul>
-				</div>
 				<!-- Avatar -->
 				<div class="dropdown">
 					<a class="dropdown-toggle d-flex align-items-center hidden-arrow" href="#" id="navbarDropdownMenuAvatar"
@@ -88,6 +66,36 @@
 		<!-- Container wrapper -->
 	</nav>
 	<!-- Navbar -->
+
+	<div class="training__programs">
+		<div class="container">
+			<h1 class="mb-5">Мои покупки</h1>
+			<div class="row gy-4 justify-content-md-center">
+				<div v-for="train in train_beginner" class="col-6 col-md-4">
+					<!-- service-item -->
+					<nuxt-link :to="'/trainings/' + train.id" class="service">
+						<!-- <img class="img-fluid" :src="train.media.images.full" alt=""> -->
+						<nuxt-img class="img-fluid" format="webp"
+							:src="train.media.images.full || '/_nuxt/assets/img/services/service_man.jpg'"
+							sizes="sm:100vw md:100vw lg:600px" />
+	
+						<span class="badge bg-secondary position-absolute top-0 start-0 m-3">Питание</span>
+						<div class="service__wrapper">
+	
+							<h3 class="service__title">{{ train.title }}</h3>
+							<svg class="align-self-end" width="30" height="30" viewBox="0 0 30 30" fill="none"
+								xmlns="http://www.w3.org/2000/svg">
+								<circle cx="15" cy="15" r="15" fill="#F52626" />
+								<path d="M13 9L19 15L13 21" stroke="white" stroke-width="2" stroke-linejoin="round" />
+							</svg>
+	
+						</div>
+					</nuxt-link>
+					<!-- end service-item -->
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script lang="ts" setup>
@@ -99,6 +107,21 @@
 const user: any = useCookie('user').value
 
 console.log(user);
+
+import { trainings } from "~/data";
+
+const train_beginner = computed(() => trainings.trains.filter((i: {
+	type: string; complexity: string;
+}) => i.complexity == 'beginner' && i.type == 'men'))
+const train_master = computed(() => trainings.trains.filter((i: {
+	type: string; complexity: string;
+}) => i.complexity == 'advanced' && i.type == 'men'))
+const train_beginner_w = computed(() => trainings.trains.filter((i: {
+	type: string; complexity: string;
+}) => i.complexity == 'beginner' && i.type == 'women'))
+const train_advanced_w = computed(() => trainings.trains.filter((i: {
+	type: string; complexity: string;
+}) => i.complexity == 'advanced' && i.type == 'women'))
 
 
 </script>
