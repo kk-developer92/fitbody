bb6dfd4aa45fece4ea0f5724b4797667c1a7e3c3
ShukurovOzodbek <shukurov.ozodbek.jaloliddinovich@gmail.com>
2023-12-13 20:27:44 +0500

    added header

---
+++ components/navbar.vue
@@ -0,0 +1,72 @@
+<template>
+    <nav id="main-navbar " class="navbar navbar-expand-lg navbar-light bg-dark pt-2">
+        <div class="container-fluid">
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <nuxt-link class="nav-link" href="/">Главная</nuxt-link>
+                    </li>
+                </ul>
+            </div>
+            <div class="d-flex align-items-center p-2">
+                <div class="">
+                    <nuxt-link class="account" to="/account" v-if="user.name">
+                        <span class="">{{ user.name }}</span>
+                        <img class="placeholder"
+                             src="https://avatars.mds.yandex.net/i?id=1263002c81e51f3c256d8cafee645c0e0fd34fc1-10803272-images-thumbs&n=13"
+                             alt="">
+                    </nuxt-link>
+                    <div class="header-reg" v-else>
+                        <nuxt-link to="/login">Войти</nuxt-link>
+                        <nuxt-link to="/signup">Регистрироваться</nuxt-link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script setup lang="ts">
+
+import axios from "axios";
+
+const cookie: any = useCookie('token');
+const url = import.meta.env.VITE_API_URL;
+
+const user: any = ref({});
+
+async function fetchUser() {
+    if (cookie.value) {
+        const token: any = parseJwt(cookie.value);
+        const usr = await axios.get(url + '/users/' + token._id);
+        user.value = usr.data;
+    }
+}
+
+fetchUser();
+
+</script>
+
+<style scoped>
+.header-reg {
+    display: flex;
+    gap: 15px;
+}
+
+a {
+    color: white;
+    text-decoration: none;
+}
+
+.placeholder {
+    border-radius: 100%;
+    width: 40px;
+}
+
+.account {
+    display: flex;
+    gap: 15px;
+    align-items: center;
+    justify-content: center;
+}
+</style>
--- pages/account/index.vue
+++ pages/account/index.vue
@@ -1,72 +1,5 @@
 <template>
-    <!-- Navbar -->
-    <nav id="main-navbar " class="navbar navbar-expand-lg navbar-light bg-dark pt-2">
-        <!-- Container wrapper -->
-        <div class="container-fluid">
-            <!-- Toggle button -->
-            <button class="navbar-toggler" type="button" data-mdb-toggle="collapse"
-                    data-mdb-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                     class="bi bi-three-dots-vertical" viewBox="0 0 16 16">
-                    <path
-                        d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"/>
-                </svg>
-            </button>
-
-            <!-- Collapsible wrapper -->
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <!-- Navbar brand -->
-                <nuxt-link class="navbar-brand mt-2 mt-lg-0 bg-light" to="/">
-                    <img src="@/assets/appImages/logo.png" height="47" alt="MDB Logo" loading="lazy"/>
-                </nuxt-link>
-                <!-- Left links -->
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <nuxt-link class="nav-link" href="/account">Главная</nuxt-link>
-                    </li>
-                    <li class="nav-item">
-                        <nuxt-link class="nav-link" href="/results">Результаты</nuxt-link>
-                    </li>
-                    <li class="nav-item">
-                        <nuxt-link class="nav-link" href="/">Еще что-то</nuxt-link>
-                    </li>
-                </ul>
-                <!-- Left links -->
-            </div>
-            <!-- Collapsible wrapper -->
-
-            <!-- Right elements -->
-            <div class="d-flex align-items-center">
-
-                <!-- Avatar -->
-                <div class="dropdown">
-                    <a class="dropdown-toggle d-flex align-items-center hidden-arrow" href="#"
-                       id="navbarDropdownMenuAvatar"
-                       role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        <img src="https://mdbcdn.b-cdn.net/img/new/avatars/2.webp" class="rounded-circle" height="25"
-                             alt="Black and White Portrait of a Man" loading="lazy"/>
-                    </a>
-                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownMenuAvatar">
-                        <li>
-                            <a class="dropdown-item" href="#">My profile</a>
-                        </li>
-                        <li>
-                            <a class="dropdown-item" href="#">Settings</a>
-                        </li>
-                        <li>
-                            <a class="dropdown-item" href="#">Logout</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-            <!-- Right elements -->
-        </div>
-        <!-- Container wrapper -->
-    </nav>
-    <!-- Navbar -->
-
+    <navbar />
     <div class="training__programs">
         <div class="container">
             <h1 class="mb-5">Мои покупки</h1>
@@ -114,6 +47,7 @@
 <script lang="ts" setup>
 import TabBlock from '~/components/common/TabBlock.vue';
 import axios from "axios";
+import Navbar from "~/components/navbar.vue";
 
 definePageMeta({
     authRoute: true,
