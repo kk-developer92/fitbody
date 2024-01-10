1385d04fb61f59460d7173c72d8cdbcee59ab93c
ShukurovOzodbek <shukurov.ozodbek.jaloliddinovich@gmail.com>
2024-01-10 15:30:21 +0500

    payment is in working state

--- pages/payment/[slug].vue
+++ pages/payment/[slug].vue
@@ -1,7 +1,7 @@
 <template>
     <div class="loginBlock">
         <div class="blur">
-            <form method="POST" class="form p-3" action="https://test.paycom.uz">
+            <form method="POST" class="form p-3" action="https://checkout.paycom.uz">
                 <h1 class="mb-4">Оплата</h1>
                 <error-block @close="closeError" class="mb-4" v-if="showError" :text="error"/>
                 <input type="hidden" name="merchant" value="658ea2865c8188fb6e915fb1"/>
@@ -10,6 +10,7 @@
                 <input type="hidden" name="account[order_id]" :value="Math.floor(Math.random() * 10000000000)"/>
                 <input type="hidden" name="account[user_id]" :value="user"/>
                 <input type="hidden" name="account[product_id]" :value="product_id"/>
+                <input type="hidden" name="callback" value="https://fitbody.uz/account"/>
                 <button type="submit" class="btn btn-primary button mt-3">Оплатить через Payme</button>
             </form>
         </div>
@@ -21,7 +22,7 @@ const showError = ref(false);
 const error = ref('');
 
 definePageMeta({
-   layout: false
+    layout: false
 });
 
 const product_id = useRoute().params.slug;
