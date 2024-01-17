6599754b0673c965097d9c094162bc81726af9a5
ShukurovOzodbek <shukurov.ozodbek.jaloliddinovich@gmail.com>
2024-01-17 13:08:03 +0500

    adding result

--- components/infoBlock.vue
+++ components/infoBlock.vue
@@ -9,7 +9,7 @@
                 <play-button/>
             </div>
             <div class="flex-grow-1 ms-3">
-                <div class="row">
+                <div class="row d-flex align-items-center">
                     <div class="col-sm">
                         <h6 class="mb-0 text-white">{{ props.train.title }}</h6>
                         <p class="mb-0">{{ props.train.repEach || 12 }} раз</p>
@@ -17,7 +17,9 @@
                     <div class="col-sm-auto flex-shrink-0 d-flex gap-3 align-items-center justify-content-between">
                         <div>
                             <h6 class="mb-0 text-white">Результат:</h6>
-                            <p class="mb-0 small">58x12,12,12</p>
+                            <div class="results" v-for="rep in reps">
+                                <p class="mb-0 small">{{rep}}x{{ props.train?.result[rep] || 0}}раз</p>
+                            </div>
                         </div>
                         <button data-bs-target="#resultModal" data-bs-toggle="modal" @click="openResModal"
                                 class="btn btn-link p-0 text-danger">
@@ -35,21 +37,21 @@
 import EditBtn from "~/components/icons/editBtn.vue";
 import PlayButton from "~/components/icons/PlayButton.vue";
 import {useModal} from "~/composables/useModal";
-import axios from "axios";
 
 const props = defineProps<{ train: any }>()
 
 const modal = useModal('videoModal');
 const resmodal = useModal('resultModal');
-
+const reps = ref(+props.train.repEach.split('x')[0]);
 
 function openModal() {
     modal.open(props.train.video)
 }
 
 function openResModal() {
-    resmodal.open()
+    resmodal.open(props.train);
 }
+
 
 </script>
 
--- components/modals/ResultModal.vue
+++ components/modals/ResultModal.vue
@@ -1,16 +1,19 @@
 <template>
     <modal class="modal fade" id="resultModal" @shown="shown">
-        <form class="form p-3">
-            <div class="row" v-for="result in 3">
+        <form class="form p-3" @submit.prevent="submit">
+            <div class="row" v-for="result in reps">
                 <div class="col-4">
                     <div class="form-floating mb-3">
-                        <input type="text" class="form-control" id="floatingInput" placeholder="Вес" inputmode="numeric">
+                        <input type="text" class="form-control" id="floatingInput" placeholder="Вес"
+                               inputmode="numeric">
                         <label for="floatingInput" :style="{color: 'gray'}">Вес</label>
                     </div>
                 </div>
                 <div class="col">
                     <div class="form-floating">
-                        <input type="text" class="form-control" id="floatingSet" placeholder="Подходы" inputmode="numeric">
+                        <input type="text" class="form-control" id="floatingSet" v-model="exact.result[result]"
+                               placeholder="Подходы"
+                               inputmode="numeric">
                         <label for="floatingSet" :style="{color: 'gray'}">Повторения</label>
                     </div>
                 </div>
@@ -22,11 +25,76 @@
 
 <script lang="ts" setup>
 import Modal from "~/components/common/Modal.vue";
+import axios from "axios";
 
 const result: any = ref();
+const URL = import.meta.env.VITE_API_URL;
+const id = useRoute().params.id;
+const slug = useRoute().params.slug;
+const path = useRoute().fullPath.split('/')[1];
+const training: any = ref({});
+const exercise: any = ref({});
+const exact: any = ref({});
+const reps = ref(0);
+const user: any = ref({});
 
 function shown(data: any) {
-    result.value = data
+    result.value = data;
+    getExercise();
+    reps.value = +exact.value.repEach.split('x')[0]
+}
+
+const cookie: any = useCookie('token');
+
+async function fetch() {
+
+    if (cookie.value) {
+        const token: any = parseJwt(cookie.value);
+        try {
+            const usr = await axios.get(URL + '/users/' + token._id);
+            user.value = usr.data;
+        } catch (e) {
+            user.value = {};
+        }
+    }
+
+    for (let i of user.value[path]) {
+        if (i._id === slug) {
+            training.value = i;
+            getDay();
+        }
+    }
+}
+
+fetch();
+
+function getDay() {
+    for (let week of training.value.exercises) {
+        for (let day of week.data) {
+            if (day._id === id) {
+                exercise.value = day
+            }
+        }
+    }
+}
+
+function getExercise() {
+    for (let i of exercise.value.trainings) {
+        for (let exercises of i.exercises) {
+            if (result.value._id === exercises._id) {
+                exact.value = exercises
+                exact.value.result = {};
+            }
+        }
+    }
+}
+
+
+async function submit() {
+    if (cookie.value) {
+        const token: any = parseJwt(cookie.value);
+        await axios.patch(`${URL}/users/${token._id}`, user.value);
+    }
 }
 
 </script>
