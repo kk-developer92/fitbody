10e8742683e8dee7a442bc87facdd4701f58ef47
ShukurovOzodbek <shukurov.ozodbek.jaloliddinovich@gmail.com>
2024-01-28 18:41:25 +0500

    animative changing page

--- pages/courses/[slug]/index.vue
+++ pages/courses/[slug]/index.vue
@@ -4,104 +4,106 @@
         <Meta name="description" :content="current_course.description"/>
     </Head>
     <div class="">
-        <main v-if="!isPurchased" class="page">
-            <section class="promo page__promo py-3 py-md-5">
-                <div class="container py-3">
-                    <div class="promo__wrapper row align-items-center">
-                        <div class="promo__image col-lg-5 col-xl-5 order-lg-last text-lg-end">
-                            <!-- <img :src="current_course.media.images.full || '/_nuxt/assets/img/services/page_img.jpg' " alt=""> -->
-                            <img :src="current_course.image"/>
-                        </div>
-                        <div class="col-lg-7 col-xl-7">
-                            <button @click="$router.go(-1)" class="page-nav">
-                                <svg width="19" height="8" viewBox="0 0 19 8" fill="none"
-                                     xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M1 4H19M1 4L4 1M1 4L4 7" stroke="currentColor"/>
-                                </svg>
-                                Назад
-                            </button>
-                            <h1>{{ current_course.title }}</h1>
-                            <div v-html="current_course.description" class="promo__text"></div>
+        <transition name="form">
+            <main v-if="!isPurchased" class="page">
+                <section class="promo page__promo py-3 py-md-5">
+                    <div class="container py-3">
+                        <div class="promo__wrapper row align-items-center">
+                            <div class="promo__image col-lg-5 col-xl-5 order-lg-last text-lg-end">
+                                <!-- <img :src="current_course.media.images.full || '/_nuxt/assets/img/services/page_img.jpg' " alt=""> -->
+                                <img :src="current_course.image"/>
+                            </div>
+                            <div class="col-lg-7 col-xl-7">
+                                <button @click="$router.go(-1)" class="page-nav">
+                                    <svg width="19" height="8" viewBox="0 0 19 8" fill="none"
+                                         xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M1 4H19M1 4L4 1M1 4L4 7" stroke="currentColor"/>
+                                    </svg>
+                                    Назад
+                                </button>
+                                <h1>{{ current_course.title }}</h1>
+                                <div v-html="current_course.description" class="promo__text"></div>
 
-                            <purchase-button :price="current_course.price" service="courses"
-                                             class="btn btn-primary button"/>
-                        </div>
+                                <purchase-button :price="current_course.price" service="courses"
+                                                 class="btn btn-primary button"/>
+                            </div>
 
-                    </div>
-                </div>
-            </section>
-
-            <div class="container">
-                <section class="page__desc">
-                    <div class="row">
-                        <div class="col-lg-8 mx-auto">
-                            <h2>О ПРОГРАММЕ</h2>
-                            <div v-html="current_course?.about_program"></div>
-
-                            <purchase-button :price="current_course.price" service="courses"
-                                             class="btn btn-primary button"/>
                         </div>
                     </div>
                 </section>
 
-                <section class="others">
-                    <h3 class="others__header">С этой программой также покупают</h3>
-                    <div class="py-4">
-                        <h2 class="text-center mb-md-4">Питание</h2>
-                        <div class="row justify-content-center">
-                            <div v-for="nutrition in filterByGender(nutrions).splice(0, 2)" class="col-6 col-md-4">
-                                <!-- service-item -->
-                                <nuxt-link :to="'/nutritions/' + nutrition._id" class="service">
-                                    <img class="img-fluid" :src="nutrition.image || image" alt="">
-                                    <div class="service__wrapper">
+                <div class="container">
+                    <section class="page__desc">
+                        <div class="row">
+                            <div class="col-lg-8 mx-auto">
+                                <h2>О ПРОГРАММЕ</h2>
+                                <div v-html="current_course?.about_program"></div>
 
-                                        <h3 class="service__title">{{ nutrition.title }}</h3>
-                                        <svg width="30" height="30" viewBox="0 0 30 30" fill="none"
-                                             xmlns="http://www.w3.org/2000/svg">
-                                            <circle cx="15" cy="15" r="15" fill="#F52626"/>
-                                            <path d="M13 9L19 15L13 21" stroke="white" stroke-width="2"
-                                                  stroke-linejoin="round"/>
-                                        </svg>
-
-                                    </div>
-                                </nuxt-link>
-                                <!-- end service-item -->
+                                <purchase-button :price="current_course.price" service="courses"
+                                                 class="btn btn-primary button"/>
                             </div>
                         </div>
-                    </div>
+                    </section>
 
-                    <div class="py-4">
-                        <h2 class="text-center mb-md-4">Тренировки</h2>
-                        <div class="row justify-content-center">
-                            <div v-for="train in filterByGender(trainings).splice(0, 2)" class="col-6 col-md-4">
-                                <!-- service-item -->
-                                <nuxt-link :to="'/trainings/' + train._id" class="service">
-                                    <img class="img-fluid" :src="train.image || image" alt="">
-                                    <div class="service__wrapper">
+                    <section class="others">
+                        <h3 class="others__header">С этой программой также покупают</h3>
+                        <div class="py-4">
+                            <h2 class="text-center mb-md-4">Питание</h2>
+                            <div class="row justify-content-center">
+                                <div v-for="nutrition in filterByGender(nutrions).splice(0, 2)" class="col-6 col-md-4">
+                                    <!-- service-item -->
+                                    <nuxt-link :to="'/nutritions/' + nutrition._id" class="service">
+                                        <img class="img-fluid" :src="nutrition.image || image" alt="">
+                                        <div class="service__wrapper">
 
-                                        <div>
-                                            <h3 class="service__title">{{ train.title }}</h3>
+                                            <h3 class="service__title">{{ nutrition.title }}</h3>
+                                            <svg width="30" height="30" viewBox="0 0 30 30" fill="none"
+                                                 xmlns="http://www.w3.org/2000/svg">
+                                                <circle cx="15" cy="15" r="15" fill="#F52626"/>
+                                                <path d="M13 9L19 15L13 21" stroke="white" stroke-width="2"
+                                                      stroke-linejoin="round"/>
+                                            </svg>
+
                                         </div>
-                                        <svg class="align-self-end" width="30" height="30" viewBox="0 0 30 30"
-                                             fill="none"
-                                             xmlns="http://www.w3.org/2000/svg">
-                                            <circle cx="15" cy="15" r="15" fill="#F52626"/>
-                                            <path d="M13 9L19 15L13 21" stroke="white" stroke-width="2"
-                                                  stroke-linejoin="round"/>
-                                        </svg>
-
-                                    </div>
-                                </nuxt-link>
-                                <!-- end service-item -->
+                                    </nuxt-link>
+                                    <!-- end service-item -->
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </section>
+
+                        <div class="py-4">
+                            <h2 class="text-center mb-md-4">Тренировки</h2>
+                            <div class="row justify-content-center">
+                                <div v-for="train in filterByGender(trainings).splice(0, 2)" class="col-6 col-md-4">
+                                    <!-- service-item -->
+                                    <nuxt-link :to="'/trainings/' + train._id" class="service">
+                                        <img class="img-fluid" :src="train.image || image" alt="">
+                                        <div class="service__wrapper">
+
+                                            <div>
+                                                <h3 class="service__title">{{ train.title }}</h3>
+                                            </div>
+                                            <svg class="align-self-end" width="30" height="30" viewBox="0 0 30 30"
+                                                 fill="none"
+                                                 xmlns="http://www.w3.org/2000/svg">
+                                                <circle cx="15" cy="15" r="15" fill="#F52626"/>
+                                                <path d="M13 9L19 15L13 21" stroke="white" stroke-width="2"
+                                                      stroke-linejoin="round"/>
+                                            </svg>
+
+                                        </div>
+                                    </nuxt-link>
+                                    <!-- end service-item -->
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                </div>
+            </main>
+            <div v-else>
+                <training-index service="courses"/>
             </div>
-        </main>
-        <div v-else>
-            <training-index service="courses"/>
-        </div>
+        </transition>
     </div>
 </template>
 
--- pages/nutritions/[slug].vue
+++ pages/nutritions/[slug].vue
@@ -3,108 +3,114 @@
         <Title>Программа питание {{ current_nutrion.title }} - Fitbody</Title>
         <Meta name="description" :content="current_nutrion.description"/>
     </Head>
-    <main class="page" v-if="!isPurchased">
-        <section class="promo page__promo py-3 py-md-5">
-            <div class="container py-3">
-                <div class="promo__wrapper row align-items-center">
-                    <div class="promo__image col-lg-5 col-xl-5 order-lg-last text-lg-end">
-                        <img :src="current_nutrion.image || '/_nuxt/assets/img/services/page_img.jpg'" alt="">
-                    </div>
-                    <div class="col-lg-7 col-xl-7">
-                        <button @click="$router.go(-1)" class="page-nav">
-                            <svg width="19" height="8" viewBox="0 0 19 8" fill="none"
-                                 xmlns="http://www.w3.org/2000/svg">
-                                <path d="M1 4H19M1 4L4 1M1 4L4 7" stroke="currentColor"/>
-                            </svg>
-                            Назад
-                        </button>
-                        <h1>{{ current_nutrion.title }}</h1>
-                        <div v-html="current_nutrion.description" class="promo__text"></div>
+    <transition name="form">
 
-                        <purchase-button :price="current_nutrion.price" service="nutrition"/>
-                    </div>
+        <main class="page" v-if="!isPurchased">
+            <section class="promo page__promo py-3 py-md-5">
+                <div class="container py-3">
+                    <div class="promo__wrapper row align-items-center">
+                        <div class="promo__image col-lg-5 col-xl-5 order-lg-last text-lg-end">
+                            <img :src="current_nutrion.image || '/_nuxt/assets/img/services/page_img.jpg'" alt="">
+                        </div>
+                        <div class="col-lg-7 col-xl-7">
+                            <button @click="$router.go(-1)" class="page-nav">
+                                <svg width="19" height="8" viewBox="0 0 19 8" fill="none"
+                                     xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M1 4H19M1 4L4 1M1 4L4 7" stroke="currentColor"/>
+                                </svg>
+                                Назад
+                            </button>
+                            <h1>{{ current_nutrion.title }}</h1>
+                            <div v-html="current_nutrion.description" class="promo__text"></div>
 
-                </div>
-            </div>
-        </section>
-
-        <div class="container">
-            <section class="page__desc">
-                <div class="row">
-                    <div class="col-lg-8 mx-auto">
-                        <h2>О ПРОГРАММЕ</h2>
-                        <div v-html="current_nutrion?.about_program"></div>
-
-                        <purchase-button :price="current_nutrion.price" service="nutrition"/>
+                            <purchase-button :price="current_nutrion.price" service="nutrition"/>
+                        </div>
 
                     </div>
                 </div>
             </section>
 
-            <section class="others">
-                <h3 class="others__header">С этой программой также покупают</h3>
-                <div class="py-4">
-                    <h2 class="text-center mb-md-4">Тренировки</h2>
-                    <div class="row justify-content-center">
-                        <div v-for="train in filterByGender(trainigs).splice(0, 2)" class="col-6 col-md-4">
-                            <!-- service-item -->
-                            <nuxt-link :to="'/trainings/' + train._id" class="service">
-                                <img class="img-fluid" :src="train.image || image" alt="">
-                                <div class="service__wrapper">
+            <div class="container">
+                <section class="page__desc">
+                    <div class="row">
+                        <div class="col-lg-8 mx-auto">
+                            <h2>О ПРОГРАММЕ</h2>
+                            <div v-html="current_nutrion?.about_program"></div>
 
-                                    <h3 class="service__title">{{ train.title }}</h3>
-                                    <svg width="30" height="30" viewBox="0 0 30 30" fill="none"
-                                         xmlns="http://www.w3.org/2000/svg">
-                                        <circle cx="15" cy="15" r="15" fill="#F52626"/>
-                                        <path d="M13 9L19 15L13 21" stroke="white" stroke-width="2"
-                                              stroke-linejoin="round"/>
-                                    </svg>
+                            <purchase-button :price="current_nutrion.price" service="nutrition"/>
 
-                                </div>
-                            </nuxt-link>
-                            <!-- end service-item -->
                         </div>
                     </div>
-                </div>
+                </section>
 
-                <div class="py-4">
-                    <h2 class="text-center mb-md-4">Курсы</h2>
-                    <div class="row justify-content-center">
-                        <div v-for="course in filterByGender(courses).splice(0, 2)" class="col-6 col-md-4">
-                            <!-- service-item -->
-                            <nuxt-link :to="'/courses/' + course._id" class="service">
-                                <img class="img-fluid" :src="course.image || image" alt="">
-                                <div class="service__wrapper">
+                <section class="others">
+                    <h3 class="others__header">С этой программой также покупают</h3>
+                    <div class="py-4">
+                        <h2 class="text-center mb-md-4">Тренировки</h2>
+                        <div class="row justify-content-center">
+                            <div v-for="train in filterByGender(trainigs).splice(0, 2)" class="col-6 col-md-4">
+                                <!-- service-item -->
+                                <nuxt-link :to="'/trainings/' + train._id" class="service">
+                                    <img class="img-fluid" :src="train.image || image" alt="">
+                                    <div class="service__wrapper">
 
-                                    <div>
-                                        <h3 class="service__title">{{ course.title }}</h3>
-                                        <div class="service__desc">{{ course.short_description }}</div>
+                                        <h3 class="service__title">{{ train.title }}</h3>
+                                        <svg width="30" height="30" viewBox="0 0 30 30" fill="none"
+                                             xmlns="http://www.w3.org/2000/svg">
+                                            <circle cx="15" cy="15" r="15" fill="#F52626"/>
+                                            <path d="M13 9L19 15L13 21" stroke="white" stroke-width="2"
+                                                  stroke-linejoin="round"/>
+                                        </svg>
+
                                     </div>
-                                    <svg class="align-self-end" width="30" height="30" viewBox="0 0 30 30" fill="none"
-                                         xmlns="http://www.w3.org/2000/svg">
-                                        <circle cx="15" cy="15" r="15" fill="#F52626"/>
-                                        <path d="M13 9L19 15L13 21" stroke="white" stroke-width="2"
-                                              stroke-linejoin="round"/>
-                                    </svg>
-
-                                </div>
-                            </nuxt-link>
-                            <!-- end service-item -->
+                                </nuxt-link>
+                                <!-- end service-item -->
+                            </div>
                         </div>
                     </div>
 
-                    <nuxt-link to="/courses" class="btn btn-link btn-more">
-                        Посмотреть все курсы
-                        <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <circle cx="8" cy="8" r="8" transform="rotate(90 8 8)" fill="#F52626"/>
-                            <path d="M11 7L8 10L5 7" stroke="white" stroke-width="0.933333" stroke-linejoin="round"/>
-                        </svg>
-                    </nuxt-link>
-                </div>
-            </section>
-        </div>
-    </main>
-    <nutrition-purchased v-else/>
+                    <div class="py-4">
+                        <h2 class="text-center mb-md-4">Курсы</h2>
+                        <div class="row justify-content-center">
+                            <div v-for="course in filterByGender(courses).splice(0, 2)" class="col-6 col-md-4">
+                                <!-- service-item -->
+                                <nuxt-link :to="'/courses/' + course._id" class="service">
+                                    <img class="img-fluid" :src="course.image || image" alt="">
+                                    <div class="service__wrapper">
+
+                                        <div>
+                                            <h3 class="service__title">{{ course.title }}</h3>
+                                            <div class="service__desc">{{ course.short_description }}</div>
+                                        </div>
+                                        <svg class="align-self-end" width="30" height="30" viewBox="0 0 30 30"
+                                             fill="none"
+                                             xmlns="http://www.w3.org/2000/svg">
+                                            <circle cx="15" cy="15" r="15" fill="#F52626"/>
+                                            <path d="M13 9L19 15L13 21" stroke="white" stroke-width="2"
+                                                  stroke-linejoin="round"/>
+                                        </svg>
+
+                                    </div>
+                                </nuxt-link>
+                                <!-- end service-item -->
+                            </div>
+                        </div>
+
+                        <nuxt-link to="/courses" class="btn btn-link btn-more">
+                            Посмотреть все курсы
+                            <svg width="16" height="16" viewBox="0 0 16 16" fill="none"
+                                 xmlns="http://www.w3.org/2000/svg">
+                                <circle cx="8" cy="8" r="8" transform="rotate(90 8 8)" fill="#F52626"/>
+                                <path d="M11 7L8 10L5 7" stroke="white" stroke-width="0.933333"
+                                      stroke-linejoin="round"/>
+                            </svg>
+                        </nuxt-link>
+                    </div>
+                </section>
+            </div>
+        </main>
+        <nutrition-purchased v-else/>
+    </transition>
 </template>
 
 <script setup lang="ts">
